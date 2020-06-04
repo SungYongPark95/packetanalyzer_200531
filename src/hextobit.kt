12c344134e3fc473f6bbffba1827fbec25d3ace2
SungYongPark95 <psy.20.2014@gmail.com>
2020-06-04 14:28:45 +0900

    Finish Ethernet parket, divide to 4 type at Ethernet Packet

--- src/hextobit.kt
+++ src/hextobit.kt
@@ -6,45 +6,60 @@ fun main() {
     var hexTest:String = readLine()!!
 
     //2. Designate each range for slicing (Ethernet Part)
-    var lengEthDestAddr = IntRange(0,11)
-    var lengEthSourAddr = IntRange(12,23)
+    val lengEthDestAddr = IntRange(0,11)
+    val lengEthSourAddr = IntRange(12,23)
 //    var lengEthAddr = 12
-    var lengEthType = IntRange(24,27)
+    val lengEthType = IntRange(24,27)
 
     //3. Slice the each part at Ethernet
-    var ethDestAddr = hexTest.slice(lengEthDestAddr)
-    var ethSourAddr = hexTest.slice(lengEthSourAddr)
+    val ethDestAddr = hexTest.slice(lengEthDestAddr)
+    val ethSourAddr = hexTest.slice(lengEthSourAddr)
 //    println(hexTest.slice(lengEthAddr))
-    var ethType = hexTest.slice(lengEthType)
+    val ethType = hexTest.slice(lengEthType)
+    hexTest = hexTest.substring(28)
 
 
-    print("1) Destination Address : ")
-    var builder : StringBuilder? = StringBuilder()
-    var buildCast : StringBuilder? = StringBuilder()
-    builder?.append(ethDestAddr)
-    for(i in 2..15 step 3)
-    builder?.insert(i,":")
-    print(builder)
-//  if (builder.toString())
-    println(castUni(builder.toString().substring(1,2)))
+    print("1.Ethernet\n\t1) Destination Address : ")
+    addrView(ethDestAddr)
 
-    print("2) Source Address : ")
-    builder?.setLength(0);
-    builder?.append(ethSourAddr)
-    for(i in 2..15 step 3)
-        builder?.insert(i,":")
-    print(builder)
-    println(castUni(builder.toString().substring(1,2)))
-    
+    print("\t2) Source Address : ")
+    addrView(ethSourAddr)
 
+    print("\t3) Type : " + ethType)
     if (ethType.equals("0800")){
+        println(" / IP\n" +
+                "2.IP\n" +
+                "\t1) Version : 04\n" +
+                "\t2) Header Length : ")
+
+
+    }else if(ethType.equals("0806")){
+        println(" / ARP\n" +
+                "2.ARP")
+    }
+    else if(ethType.equals("86dd")){
+        println(" / IPv6\n" +
+                "2.IPv6\n" +
+                "\t1) Version : 06")
+    }else{
+        println(" / Ethernet or Other kinds of Protocol")
     }
 }
 
 fun castUni(i :String): String {
-    var a = i.toInt(16)
-    if (a % 2 ==0)
+    val a = i.substring(1,2).toInt(16)
+    if (i.equals("ff:ff:ff:ff:ff:ff"))
+        return " / Broadcast"
+    if (a % 2 == 0)
         return " / Unicast"
     else
         return " / Multicast"
+}
+
+fun addrView(addr : String){
+    var builder : StringBuilder? = StringBuilder()
+    builder?.append(addr)
+    for(i in 2..15 step 3)
+        builder?.insert(i,":")
+    println(builder?.append(castUni(builder.toString())))
 }
