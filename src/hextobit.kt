28912abfe26788bf1de420d9a3d563315117a4d4
SungYongPark95 <rlrmadydwls@hs.ac.kr>
2020-06-09 18:34:21 +0900

    Until ICMP, Need to do : UDP, OSPF, IPv6

--- src/hextobit.kt
+++ src/hextobit.kt
@@ -2,6 +2,7 @@ import java.lang.String.format
 import java.lang.StringBuilder
 import kotlin.system.exitProcess
 
+// 과정중에 16진수로 제대로 변환하지 않은 곳이 있는지 확인할 것
 fun main() {
     //1.Get the packet from user
     println("Input the packet!")
@@ -37,7 +38,7 @@ fun main() {
         var hexTemp = slicePair(hexT, 1, 2)
 //        val ipHeaderLeng = hexT.substring(1,2)
 //        hexT = hexT.substring(2)
-        var ipheaderLeng = ipv4headerLeng(hexTemp.first)
+        val ipheaderLeng = ipv4headerLeng(hexTemp.first)
         hexTemp = slicePair(hexTemp.second, 0,2)
         print("\t3) Service Type : "+ hexTemp.first)
         if(hexTemp.first == "00"){
@@ -47,12 +48,12 @@ fun main() {
             ipv4ToS(hexTemp.first)
         }
         hexTemp = slicePair(hexTemp.second, 0, 4)
-        var ipv4TotalLeng = hexTemp.first.toInt(radix = 16)
-        var ipv4Payload = ipv4TotalLeng - ipheaderLeng
+        val ipv4TotalLeng = hexTemp.first.toInt(radix = 16)
+        val ipv4Payload = ipv4TotalLeng - ipheaderLeng
         println("\t4) Total Length : "+ hexTemp.first + " / " + ipv4TotalLeng + " bytes : " + ipv4Payload + " bytes payload")
 
         hexTemp = slicePair(hexTemp.second, 0, 4)
-        var ipv4Iden = hexTemp.first.toInt(radix = 16)
+        val ipv4Iden = hexTemp.first.toInt(radix = 16)
         print("\t5) Identification : "+ hexTemp.first+ " / " + ipv4Iden + " /")
         ipv4IdenSlice(hexTemp.first)
 
@@ -67,10 +68,10 @@ fun main() {
         ipv4TTL(hexTemp.first)
 
         hexTemp = slicePair(hexTemp.second, 0,2)
-        var ipv4ptc = ipv4Protocol(hexTemp.first)
+        val ipv4ptc = ipv4Protocol(hexTemp.first)
 
         hexTemp = slicePair(hexTemp.second, 0,4)
-        var ipv4checksum = hexTemp.first
+        val ipv4checksum = hexTemp.first
         println("\t10) Checksum : " + ipv4checksum)
 
         hexTemp = slicePair(hexTemp.second, 0,8)
@@ -81,11 +82,117 @@ fun main() {
         print("\t12) Destination Address : ")
         ipv4addr(hexTemp.first)
 
+        if(ipv4ptc == 1)
+        {
+            println("3. ICMP")
+            hexTemp = slicePair(hexTemp.second, 0,2)
+            print("\t1) Type : " + hexTemp.first + " / ")
+            val icmpType = icmpTP(hexTemp.first)
+
+            hexTemp = slicePair(hexTemp.second, 0,2)
+            icmpCode(hexTemp.first, icmpType)
+
+            hexTemp = slicePair(hexTemp.second, 0,4)
+            val icmpChecksum = hexTemp.first
+            println("\t3) Checksum : " + icmpChecksum)
+
+            println("\t3) Rest Data : " + hexTemp.second  + " / " + (hexTemp.second.length/2) + " bytes")
+
+        }else if (ipv4ptc == 2 ){
+            println("3. IGMP")
+        }else if (ipv4ptc == 6 ){
+            println("3. TCP")
+
+            hexTemp = slicePair(hexTemp.second, 0, 4)
+            print("\t1) Source Port : ")
+            val tcpSourcePort = tcpPort(hexTemp.first)
+
+            hexTemp = slicePair(hexTemp.second, 0, 4)
+            print("\t2) Destination Port : ")
+            val tcpDestPort = tcpPort(hexTemp.first)
+
+            hexTemp = slicePair(hexTemp.second, 0, 8)
+            println("\t3) Sequence number : " + hexTemp.first)
+
+            hexTemp = slicePair(hexTemp.second, 0, 8)
+            println("\t4) Ack number : " + hexTemp.first)
+
+            hexTemp = slicePair(hexTemp.second, 0, 1)
+            var tcpHeaderLeng = tcpHeaderLeng(hexTemp.first)
+
+            hexTemp = slicePair(hexTemp.second, 1, 3)
+            tcpControlBit(hexTemp.first)
+
+            hexTemp = slicePair(hexTemp.second, 0, 4)
+            val tcpWindow = hexTemp.first.toInt(16)
+            println("\t7) Window Size : " + hexTemp.first + " / " + tcpWindow + " bytes")
+
+            hexTemp = slicePair(hexTemp.second, 0, 4)
+            val tcpChecksum = hexTemp.first
+            println("\t8) Checksum : " + tcpChecksum)
+
+            hexTemp = slicePair(hexTemp.second, 0, 4)
+            val tcpUrgPoint = hexTemp.first
+            print("\t9) Urgent Point : ")
+            if(tcpUrgPoint == "0000"){
+                println("0000 / Not Urgent")
+            }else{
+                println(tcpUrgPoint + " / Check the Urgent Point")
+            }
+
+            val tcpOpt = hexTemp.second
+            if(tcpHeaderLeng == 20){
+
+            }else{
+                println("\t10) Option : " + tcpOpt + " / " + (tcpOpt.length/2) + " bytes")
+            }
+
+        }else if (ipv4ptc == 8 ){
+            println("3. EGP")
+        }else if (ipv4ptc == 17 ){
+            println("3. UDP")
+        }else if (ipv4ptc == 89 ){
+            println("3. OSPF")
+        }
+
 
 
     }else if(ethType.equals("0806")){
         println(" / ARP\n" +
                 "2.ARP")
+        var hexTemp = slicePair(hexT, 0, 4)
+        val arpHWType = arpHWT(hexTemp.first)
+
+        hexTemp = slicePair(hexTemp.second, 0, 4)
+        val arpProtocolType = arpPTT(hexTemp.first)
+
+        hexTemp = slicePair(hexTemp.second, 0, 2)
+        val arpHWSize = arpHWS(hexTemp.first)
+
+        hexTemp = slicePair(hexTemp.second, 0, 2)
+        val arpProtocolSize = hexTemp.first.toInt(16) * 8
+        println("\t4) Protocol Size : "+ hexTemp.first + " / " + arpProtocolSize + " bits")
+
+        hexTemp = slicePair(hexTemp.second, 0, 4)
+        val arpOperation = hexTemp.first
+        arpOper(arpOperation)
+
+        hexTemp = slicePair(hexTemp.second, 0, 12)
+        print("\t6) Sender MAC Address : ")
+        addrView(hexTemp.first)
+
+        hexTemp = slicePair(hexTemp.second, 0, 8)
+        print("\t7) Sender IP Address : ")
+        ipv4addr(hexTemp.first)
+
+        hexTemp = slicePair(hexTemp.second, 0, 12)
+        print("\t8) Target MAC Address : ")
+        addrView(hexTemp.first)
+
+        hexTemp = slicePair(hexTemp.second, 0, 8)
+        print("\t9) Target IP Address : ")
+        ipv4addr(hexTemp.first)
+
     }
     else if(ethType.equals("86dd")){
         println(" / IPv6\n" +
@@ -101,6 +208,8 @@ fun castUni(i :String): String {
     val a = i.substring(1,2).toInt(16)
     if (i.equals("ff:ff:ff:ff:ff:ff"))
         return " / Broadcast"
+    if (i.equals("00:00:00:00:00:00"))
+        return " / Unknown MAC"
     if (a % 2 == 0)
         return " / Unicast"
     else
@@ -116,7 +225,7 @@ fun addrView(addr : String){
 }
 
 fun ipv4headerLeng(leng : String) : Int {
-    var intHeadLeng = leng.toInt() * 4
+    var intHeadLeng = leng.toInt(16) * 4
     if (intHeadLeng == 20){
         println(leng + " / 20 byte : No-option")
     }else if(intHeadLeng > 60){
@@ -227,5 +336,231 @@ fun ipv4addr (t: String){
         }
         print(".")
         temp = slicePair(temp.second, 0, 2)
+    }
+}
+
+fun tcpPort (t: String) : Int{
+    val tempPort = t.toInt(16)
+    print( t + " / " + tempPort)
+    if(tempPort in 0..1023){
+        print("(Well-Known Port) : ")
+        if (tempPort == 67 || tempPort == 68){
+            println("DHCP")
+        }else if(tempPort == 53){
+            println("DNS")
+        }else if(tempPort == 20){
+            println("FTP (Control)")
+        }else if(tempPort == 1){
+            println("FTP (data)")
+        }else if(tempPort == 80){
+            println("WWW (HTTP)")
+        }else if(tempPort == 443){
+            println("WWW (HTTPS)")
+        }else if(tempPort == 110){
+            println("POP3")
+        }else if(tempPort in 137..139){
+            println("SMB")
+        }else if(tempPort == 25){
+            println("SMTP")
+        }else if(tempPort == 22){
+            println("SSH")
+        }else if(tempPort == 23) {
+            println("Telnet")
+        }
+    }else if (tempPort in 1024..49151){
+        println(" : Registered Port")
+    }else if (tempPort in 49152..65535){
+        println(" : Dynamic and/or Private Port")
+    }else{
+        println(" : Unregistered Port, Please Check")
+    }
+    return tempPort
+}
+
+fun tcpHeaderLeng(t: String) : Int{
+    var temp = t.toInt(16)
+    println("\t5) Header Length : " + temp + " / " + temp * 4 + " bytes : option " + (temp * 4 - 20) + " bytes")
+    return temp * 4
+}
+
+fun tcpControlBit(t: String){
+    var splitTemp = slicePair(t, 0, 1)
+    var sa = format("%4s", splitTemp.first.toInt(16).toString(2)).replace(" ", "0")
+    var sb = format("%4s", splitTemp.second.toInt(16).toString(2)).replace(" ", "0")
+    println("\t6) Control Bits : " + t + " / "+ sa + " " + sb)
+
+    splitTemp = slicePair(sa, 2,3)
+    print("\t\t- Urgent : " + splitTemp.first + " / ")
+    if (splitTemp.first == "0"){
+        println("Not urgent")
+    } else{
+        println("Urgent")
+    }
+
+    val ack = splitTemp.second
+    print("\t\t- AcK : " + ack + " / ")
+    if (ack == "1"){
+        println("Acknowledgement")
+    } else{
+        println("Non-Acknowledgement")
+    }
+
+    splitTemp = slicePair(sb, 0,1)
+    print("\t\t- Push : " + splitTemp.first + " / ")
+    if (splitTemp.first == "0"){
+        println("Normal")
+    } else{
+        println("ASAP or No more data")
+    }
+
+    splitTemp = slicePair(splitTemp.second, 0,1)
+    print("\t\t- Reset : " + splitTemp.first + " / ")
+    if (splitTemp.first == "0"){
+        println("Normal")
+    } else{
+        println("Forced Reset or Connection Problem")
+    }
+
+    splitTemp = slicePair(splitTemp.second, 0,1)
+    val syn = splitTemp.first
+    print("\t\t- Syn : " + syn + " / ")
+    if (ack == "0" && syn == "1"){
+        println("Connection Request")
+    }else if(ack == "1" && syn == "1"){
+        println("Connection Approve")
+    }else if(ack == "1"){
+        println("Connection Setup")
+    }
+
+    val fin = splitTemp.second
+    print("\t\t- Fin : " + fin + " / ")
+    if (fin == "0"){
+        println("Not Connection Release")
+    }else if(fin == "1" && ack == "1"){
+        println("Connection Closing Response")
+    }else if(fin == "1"){
+        println("Connection Request")
+    }
+}
+
+fun arpHWT(t: String) : Int{
+    val arpHWt = t.toInt(16)
+    print("\t1) H/W Type : " + t + " / ")
+    if (arpHWt == 1){
+        println("Ethernet(10Mb)")
+    }else if (arpHWt == 2){
+        println("Experimental Ethernet(3Mb)")
+    }else if (arpHWt == 3){
+        println("Amateur Radio AX.25")
+    }else if (arpHWt == 4){
+        println("Proteon ProNET Token Ring")
+    }else if (arpHWt == 5){
+        println("Chaos")
+    }else if (arpHWt == 6){
+        println("IEEE 802.3 networks")
+    }else if (arpHWt == 7){
+        println("ARCNET")
+    }else if (arpHWt == 8){
+        println("Hyperchannel")
+    }else if (arpHWt == 9){
+        println("Lanstar")
+    }else if (arpHWt == 10){
+        println("Autonet Short Address")
+    }else if (arpHWt == 11){
+        println("LocalTalk")
+    }else if (arpHWt == 12){
+        println("LocalNet (IBM PCNet or SYTEK LocalNet")
+    }
+    return arpHWt
+}
+
+fun arpHWS(t: String) : Int{
+    val arpHWs = t.toInt(16) * 8
+    println("\t3) H/W Size : " + t + " / " + arpHWs + "bits")
+    return arpHWs
+}
+
+fun arpPTT(t: String){
+    print("\t2) Protocol Type : " + t + " / ")
+    if (t == "0800"){
+        println("IPv4")
+    }else if (t == "0806"){
+        println("ARP")
+    }else if (t == "0835"){
+        println("RARP")
+    }else if (t == "86DD"){
+        println("IPv6")
+    }else if (t == "8100"){
+        println("VLAN ID")
+    }else if (t == "8863"){
+        println("PPPoE Discovery Stage")
+    }else if (t == "8864h"){
+        println("PPPoE PPP Session Stage")
+    }
+}
+
+fun arpOper(t: String){
+    print("\t5) Operation : " + t + " / ")
+    val oper = t.toInt(16)
+    if (oper == 1){
+        println("ARP Request")
+    }else if (oper == 2){
+        println("ARP Reply")
+    }else if (oper == 3){
+        println("RARP Request")
+    }else if (oper == 4){
+        println("RARP Reply")
+    }
+}
+
+fun icmpTP(t: String) : Int{
+    val type = t.toInt(16)
+    if (type == 0){
+        print("Info Message / ")
+        println("Echo Reply")
+    } else if (type == 8){
+        print("Info Message / ")
+        println("Echo Request")
+    } else if (type == 9){
+        print("Info Message / ")
+        println("Router Advertisement")
+    } else if (type == 10){
+        print("Info Message / ")
+        println("Router Solicitation")
+    }else if (type == 3){
+        print("Error Reporting Message / ")
+        println("Destination Unreachable")
+    }else if (type == 4){
+        print("Error Reporting Message / ")
+        println("Source Quench")
+    }else if (type == 5){
+        print("Error Reporting Message / ")
+        println("Redirect")
+    }else if (type == 11){
+        print("Error Reporting Message / ")
+        println("Time Exceeded")
+    }else if (type == 12){
+        print("Error Reporting Message / ")
+        println("Parameter Problem")
+    }
+    return type
+}
+
+fun icmpCode(t: String, icmpType : Int){
+    print("\t2) Code : " + t)
+    val code = t.toInt(16)
+    if (icmpType == 3){
+        print(" / ")
+        if(code == 0){
+            println("Network Unreachable")
+        } else if(code == 1){
+            println("Host Unreachable")
+        } else if(code == 2){
+            println("Protocol Unreachable")
+        } else if(code == 3){
+            println("Port Unreachable")
+        }
+    }else{
+        println()
     }
 }
